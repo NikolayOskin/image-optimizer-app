2c47f23670a02350999349d0e1933434df5c29d6
Nikolay Oskin <nickolay.oskin@gmail.com>
2020-08-17 02:28:43 +0300

    add optimizePNG logic

--- compress.go
+++ compress.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/nickalie/go-mozjpegbin"
+	pngquant "github.com/yusukebe/go-pngquant"
 	"image/jpeg"
+	"image/png"
 	"io"
 	"io/ioutil"
 	"os"
@@ -55,10 +56,51 @@ func optimizeJPEG(path string) bool {
 	} else {
 		fmt.Println(fmt.Sprintf("--- %s", path))
 	}
-
-	return true
 }
 
-func optimizePNG(file string) {
+func optimizePNG(path string) (int64, error) {
+	finput, err := os.Open(path)
+	if err != nil {
+		return 0, err
+	}
+	input, err := ioutil.ReadAll(finput)
+	if err != nil {
+		return 0, err
+	}
+	in := bytes.NewReader(input)
+	img, err := png.Decode(in)
+	finput.Close()
+	if err != nil {
+		return 0, err
+	}
 
+	// Encode image
+	out := new(bytes.Buffer)
+	cimg, err := pngquant.Compress(img, "1")
+	if err != nil {
+		return 0, err
+	}
+	err = png.Encode(out, cimg)
+	if err != nil {
+		return 0, err
+	}
+
+	outlen := int64(out.Len())
+	if outlen < in.Size() {
+		// Write to file
+		f, err := os.Create(path)
+		if err != nil {
+			return 0, err
+		}
+		_, err = io.Copy(f, out)
+		if err != nil {
+			return 0, err
+		}
+		f.Close()
+
+		saved := (in.Size() - outlen) * 100 / in.Size()
+		return saved, nil
+	} else {
+		return 0, nil
+	}
 }
