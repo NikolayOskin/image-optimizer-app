cf4e1db4b43a51b9d2fdac2bf5dcc9dca2efc0b5
Nikolay Oskin <nickolay.oskin@gmail.com>
2020-08-24 00:41:55 +0300

    add count compressed sizes tests

--- compress_test.go
+++ compress_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -21,6 +24,51 @@ func TestByteCountSI(t *testing.T) {
 
 		if testcase.expected != result {
 			t.Errorf("returned wrong string: got %s want %s", result, testcase.expected)
+		}
+	}
+}
+
+func TestCountCompressedSizes(t *testing.T) {
+	cases := []struct {
+		inputFileSize           int64
+		outFileSize             int64
+		expectSavedAmountIsZero bool
+		expectError             bool
+	}{
+		{0, 0, true, true},
+		{1000, 1500, true, false},
+		{1000, 500, false, false},
+		{1000, 1000, true, false},
+	}
+
+	for _, testcase := range cases {
+		result := compressResult{}
+
+		tmpfile := filepath.Join(imagesPath, "tmpfile")
+		if err := ioutil.WriteFile(
+			tmpfile,
+			make([]byte, testcase.outFileSize),
+			0666,
+		); err != nil {
+			t.Errorf("can't create temp file: %v", err)
+		}
+		file, err := os.Open(tmpfile)
+		if err != nil {
+			t.Errorf("can't open tmpfile: %v", err)
+		}
+
+		err = result.countCompressedSizes(testcase.inputFileSize, file)
+		if err == nil && testcase.expectError {
+			t.Error("expected error, getting nil")
+		}
+		if err != nil && testcase.expectError == false {
+			t.Error("error is returned, but not expected")
+		}
+		if testcase.expectSavedAmountIsZero && result.saved != 0 {
+			t.Errorf("expected saved is 0, %v returned", result.saved)
+		}
+		if result.saved == 0 && !testcase.expectSavedAmountIsZero {
+			t.Error("saved is 0, expected is more than 0")
 		}
 	}
 }
