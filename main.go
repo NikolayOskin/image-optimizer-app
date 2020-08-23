cd9b5594eff5e018ab16f3ed8ad985616a12a853
Nikolay Oskin <nickolay.oskin@gmail.com>
2020-08-23 13:33:11 +0300

    remove printMemUsage helper

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"runtime"
-
 	"github.com/gorilla/sessions"
 )
 
@@ -15,18 +12,4 @@ var store *sessions.CookieStore
 func main() {
 	app := NewApp()
 	app.Run()
-}
-
-func PrintMemUsage() {
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
-	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
-	fmt.Printf("\tNumGC = %v\n", m.NumGC)
-}
-
-func bToMb(b uint64) uint64 {
-	return b / 1024 / 1024
 }
